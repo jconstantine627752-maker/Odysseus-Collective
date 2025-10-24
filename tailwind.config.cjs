d740efc922bca7acedeb0241c480ad8d93b3e631
jconstantine627752-maker <jconstantine627752@gmail.com>
2025-10-24 01:24:47 -0500

    Create  tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+```js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+```
